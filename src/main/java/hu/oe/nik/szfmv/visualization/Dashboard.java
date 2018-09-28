515d0caf660ab9e9faa6a854c6dc2bd22b39a1d6
Fülöp <fulop.anita725@gmail.com>
2018-09-28 23:11:04 +0200

    Add the KeyListener on the Gui class for push the up or down button

--- src/main/java/hu/oe/nik/szfmv/visualization/Dashboard.java
+++ src/main/java/hu/oe/nik/szfmv/visualization/Dashboard.java
@@ -8,19 +8,49 @@ import java.awt.*;
  */
 public class Dashboard extends JPanel {
 
-    private final int width = 250;
-    private final int height = 700;
-    private final int backgroundColor = 0x888888;
+	private final int width = 250;
+	private final int height = 700;
+	private final int backgroundColor = 0x888888;
+	Measurer tachometer;
+	Gui parent;
+	public int power;
 
-    /**
-     * Initialize the dashboard
-     */
-    public Dashboard() {
-        // Not using any layout manager, but fixed coordinates
-        setLayout(null);
-        setBackground(new Color(backgroundColor));
-        setBounds(770, 0, width, height);
 
-    }
+	/**
+	 * Initialize the dashboard
+	 */
+	public Dashboard(Gui pt) {
+		// Not using any layout manager, but fixed coordinates
+		setLayout(null);
+		setBackground(new Color(backgroundColor));
+		setBounds(770, 0, width, height);
+		tachometer = new Measurer(this);
+		parent = pt;
+		add(tachometer);
+		power = 0;
+		Timer.start();
+		
+	}
+
+	Thread Timer = new Thread() {
+		int newPower;
+
+		public void run() {
+			while (true) {
+				newPower = parent.newValue - 69;
+				power = newPower;
+				if (parent.newValue > 0) {
+					parent.newValue--;
+
+				}
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException ex) {
+				}
+				
+				tachometer.repaint();
+			}
+		}
+	};
 
 }
