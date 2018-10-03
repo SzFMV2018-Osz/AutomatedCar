50ce9c83b3e0da3f43bd3469e8068ded2e461212
galbavi.mate@gmail.com <galbavi.mate@gmail.com>
2018-10-03 19:40:54 +0200

    Teszt esetek készítése

--- src/main/java/hu/oe/nik/szfmv/common/Resistences.java
+++ src/main/java/hu/oe/nik/szfmv/common/Resistences.java
@@ -4,22 +4,22 @@ import java.awt.*;
 
 public final class Resistences {
 
-    private static double cDrag = 0.5;
-    private static double cRolling = 1.5;
+    private static double cDrag = 0.4257;
+    private static double cRolling = 12.8;
 
-    public static Point calculateAirResistance(Point v) {
-        double speed = Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.y, 2));
+    public static Point calculateAirResistance(Point velocityVector) {
+        double speed = Math.sqrt(Math.pow(velocityVector.x, 2) + Math.pow(velocityVector.y, 2));
         Point fDrag = new Point();
-        fDrag.x = (int) (-cDrag * v.x * speed);
-        fDrag.y = (int) (-cDrag * v.y * speed);
+        fDrag.x = (int) (-cDrag * velocityVector.x * speed);
+        fDrag.y = (int) (-cDrag * velocityVector.y * speed);
         return fDrag;
 
     }
 
-    public static Point calulateRollingResistance(Point v) {
+    public static Point calulateRollingResistance(Point velocityVector) {
         Point fRolling = new Point();
-        fRolling.x = (int) (-cRolling * v.x);
-        fRolling.y = (int) (-cRolling * v.y);
+        fRolling.x = (int) (-cRolling * velocityVector.x);
+        fRolling.y = (int) (-cRolling * velocityVector.y);
         return fRolling;
     }
 }
