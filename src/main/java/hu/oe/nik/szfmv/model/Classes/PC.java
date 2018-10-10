f063c16accbf5ae10e3c6fd399150c91d8a4e49a
bendzsi <klim.bendzsi@gmail.com>
2018-10-10 16:43:54 +0200

    #46 Interfészek létrehozása. PC interfész javítás

--- src/main/java/hu/oe/nik/szfmv/model/Classes/PC.java
+++ src/main/java/hu/oe/nik/szfmv/model/Classes/PC.java
@@ -11,7 +11,8 @@ public class PC extends Dinamikus {
      * @param y             the initial y coordinate of the object
      * @param imageFileName the filename of the image representing the object in the virtual world
      */
-    public PC(int x, int y, String imageFileName) {
-        super(x, y, imageFileName);
+    public PC(int x, int y, String imageFileName, String m11s, String m12s, String m21s, String m22s)
+    {
+        super(x, y, imageFileName,m11s,m12s,m21s,m22s);
     }
 }
