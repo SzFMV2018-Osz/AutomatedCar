988a8918bcd35c32ea990429abbb472a3451dee6
Norberg95 <43233800+Norberg95@users.noreply.github.com>
2018-10-20 00:11:19 +0200

    Szenzor Interface megvalósítása

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/sensors/ISensor.java
@@ -0,0 +1,14 @@
+package hu.oe.nik.szfmv.automatedcar.sensors;
+
+import hu.oe.nik.szfmv.environment.WorldObject;
+
+import java.awt.*;
+import java.util.List;
+
+
+public interface ISensor
+{
+    Polygon locateSensorTriangle(Point sensorPosition, double visualRange, double angelOfView, double sensorRotation);
+    void RefreshSensor(Point newSensorPosition, double newSensorRotation);
+    List<WorldObject> detectedObjects(List<WorldObject> worldObjects);
+}
