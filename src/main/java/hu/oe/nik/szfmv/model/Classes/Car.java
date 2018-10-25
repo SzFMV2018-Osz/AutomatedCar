16f5bf9b24c76627d80c2aae33b13f4dfb666b36
VECKHARDT <eckhardtvid@gmail.com>
2018-10-25 12:02:19 +0200

    fizikai modell implementálása a különböző autó modellekhez.

--- src/main/java/hu/oe/nik/szfmv/model/Classes/Car.java
+++ src/main/java/hu/oe/nik/szfmv/model/Classes/Car.java
@@ -29,6 +29,219 @@ public abstract class Car extends Dynamic implements ICollidable {
     public Car(int x, int y, String imageFileName, double m11, double m12, double m21, double m22) {
         super(x, y, imageFileName, m11, m12, m21, m22);
 
+        PhysicsModel physicsModel = new PhysicsModel();
+
+        switch(imageFileName) {
+            case "car_1_blue" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false);
+                physicsModel.setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+
+                break; // optional
+            case "car_1_red" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel. setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_1_white" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel. setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_2_blue" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_2_red" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_2_white" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_3_black" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.weight=2500;
+                physicsModel.setWidth(98);
+                physicsModel.setWeight(287);
+                break; // optional
+            case "car_1_blue_damaged" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_1_red_damaged" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel. setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_1_white_damaged" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_2_blue_damaged" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_2_red_damaged" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_2_white_damaged" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_3_black_damaged" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(false);
+                physicsModel.weight=2500;
+                physicsModel.setWidth(98);
+                physicsModel.setWeight(287);
+                break; // optional
+            case "car_1_blue_damaged_full" :
+                physicsModel.setDamage(100);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_1_red_damaged_full" :
+                physicsModel.setDamage(100);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_1_white_damaged_full" :
+                physicsModel.setDamage(100);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1400);
+                physicsModel.setWidth(95);
+                physicsModel.setWeight(238);
+                break; // optional
+            case "car_2_blue_damaged_full" :
+                physicsModel.setDamage(100);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_2_red_damaged_full" :
+                physicsModel.setDamage(100);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_2_white_damaged_full" :
+                physicsModel.setDamage(100);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(false); 
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_3_black_damaged_full" :
+                physicsModel.setDamage(100);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.weight=2500;
+                physicsModel.setWidth(98);
+                physicsModel.setWeight(287);
+                break; // optional
+            case "car_2_white_blooded" :
+                physicsModel.setDamage(0);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(true);
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            case "car_2_white_damaged_blooded" :
+                physicsModel.setDamage(50);
+                physicsModel.setDamagelimit(100);
+                physicsModel.setIsdead(true);
+                physicsModel.setFatal(true);
+                physicsModel.setWeight(1100);
+                physicsModel.setWidth(89);
+                physicsModel.setWeight(207);
+                break; // optional
+            // You can have any number of case statements.
+            default : // Optional
+                // Statements
+        }
+
     }
 
 }
