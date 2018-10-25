d6dd3ac28f7cafc715fa85913fdb36a65a966ddc
VECKHARDT <eckhardtvid@gmail.com>
2018-10-25 11:01:30 +0200

    fizikai modell létrehozása - szélesség, magasság, súly, sérülés mértéke, "halott-e" - ezek gettere és settere, területszámítás, sérülés hozzáadása, "halál" kikalkulálása

---
+++ src/main/java/hu/oe/nik/szfmv/model/Classes/PhysicsModel.java
@@ -0,0 +1,77 @@
+package hu.oe.nik.szfmv.model.Classes;
+
+public class PhysicsModel {
+   public int height;
+   public int width;
+   public int weight;
+   public int damage;
+   public int damagelimit;
+   public boolean isdead;
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public boolean isIsdead() {
+        return isdead;
+    }
+
+    public int getDamagelimit() {
+        return damagelimit;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setIsdead(boolean isdead) {
+        this.isdead = isdead;
+    }
+
+    public void setDamagelimit(int damagelimit) {
+        this.damagelimit = damagelimit;
+    }
+
+    public int getArea() {
+        return height * width;
+    }
+
+    public void addDamage(int damage) {
+        this.damage += damage;
+    }
+
+    public void CalculateDead() {
+        if(damage == damagelimit){
+         isdead=true;
+        }
+        else{
+            isdead=false;
+        }
+    }
+
+}
