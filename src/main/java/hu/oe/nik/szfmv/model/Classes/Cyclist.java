e775ac7c8fccf1a36c108235137d361f7f4e3862
VECKHARDT <eckhardtvid@gmail.com>
2018-10-25 12:11:06 +0200

    fizikai modell implementálása a biciklishez

--- src/main/java/hu/oe/nik/szfmv/model/Classes/Cyclist.java
+++ src/main/java/hu/oe/nik/szfmv/model/Classes/Cyclist.java
@@ -26,5 +26,14 @@ public class Cyclist extends Dynamic implements ICollidable {
      */
     public Cyclist(int x, int y, String imageFileName, double m11, double m12, double m21, double m22) {
         super(x, y, imageFileName, m11, m12, m21, m22);
+
+        PhysicsModel physicsModel = new PhysicsModel();
+        physicsModel.setDamage(0);
+        physicsModel.setDamagelimit(100);
+        physicsModel.setIsdead(false);
+        physicsModel.setFatal(false);
+        physicsModel.setWeight(100);
+        physicsModel.setWidth(51);
+        physicsModel.setWeight(126);
     }
 }
