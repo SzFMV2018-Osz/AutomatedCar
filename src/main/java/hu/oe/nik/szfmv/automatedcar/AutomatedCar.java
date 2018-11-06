dfc1604d14ae56e0ba1606c230bb5f74d20e9a97
Török Eszter <eszter.torok@lombiq.com>
2018-11-06 20:16:37 +0100

    Adding worldobject to virtualfunctionbus

--- src/main/java/hu/oe/nik/szfmv/automatedcar/AutomatedCar.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/AutomatedCar.java
@@ -2,6 +2,7 @@ package hu.oe.nik.szfmv.automatedcar;
 
 import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
 import hu.oe.nik.szfmv.automatedcar.bus.packets.carpacket.CarPacket;
+import hu.oe.nik.szfmv.automatedcar.sensors.CameraSensor;
 import hu.oe.nik.szfmv.automatedcar.systemcomponents.Driver;
 import hu.oe.nik.szfmv.automatedcar.systemcomponents.PowertrainSystem;
 import hu.oe.nik.szfmv.model.Classes.Car;
@@ -13,6 +14,7 @@ public class AutomatedCar extends Car {
 
     private final VirtualFunctionBus virtualFunctionBus = new VirtualFunctionBus();
     private PowertrainSystem powertrainSystem;
+    private CameraSensor cameraSensor;
 
     /**
      * Creates an object of the virtual world on the given coordinates with the given image.
@@ -27,6 +29,7 @@ public class AutomatedCar extends Car {
         setCarPacket();
 
         powertrainSystem = new PowertrainSystem(virtualFunctionBus);
+        cameraSensor = new CameraSensor(virtualFunctionBus);
 
         new Driver(virtualFunctionBus);
     }
