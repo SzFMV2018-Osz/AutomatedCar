d2b5329a35da1194528f165047f5f57488d604a5
Török Eszter <eszter.torok@lombiq.com>
2018-11-06 20:38:42 +0100

    Refactor code

--- src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/sensor/ReadOnlySensorPacket.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/sensor/ReadOnlySensorPacket.java
@@ -5,6 +5,18 @@ import hu.oe.nik.szfmv.environment.WorldObject;
 import java.util.List;
 
 public interface ReadOnlySensorPacket {
+
+    /**
+     * Gets the world object in the angle of view of the camera sensor
+     *
+     * @return List<WorldObject>
+     */
     List<WorldObject> getDetectedObjects();
+
+    /**
+     * Sets the objects of the list
+     *
+     * @param worldObjects in the angle of view of the camera sensor
+     */
     void setDetectedObjects(List<WorldObject> worldObjects);
 }
--- src/main/java/hu/oe/nik/szfmv/automatedcar/sensors/CameraSensor.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/sensors/CameraSensor.java
@@ -12,42 +12,38 @@ import java.util.List;
 public class CameraSensor extends SystemComponent implements ISensor {
 
     private static final int TRIANGLE_N = 3;
-    private Point sensorPosition;
-    Polygon triangle;
     private boolean rightLane;
     private int distanceFromBorder;
+    private Polygon triangle;
 
     /**
      * @param virtualFunctionBus This Bus help to communicate with other SystemComponent
      *                           Create Radar sensor
      */
     public CameraSensor(VirtualFunctionBus virtualFunctionBus) {
-        super(virtualFunctionBus);
+        super( virtualFunctionBus );
 
-        this.virtualFunctionBus.sensorPacket = new SensorPacket();
-        //this.sensorPosition = new Point(this.virtualFunctionBus.carPacket.getxPosition(), this.virtualFunctionBus.carPacket.getyPosition()+this.virtualFunctionBus.carPacket.getCarWidth()/2);
         triangle = new Polygon();
     }
 
     @Override
     public Polygon locateSensorTriangle(Point sensorPosition, double visualRange,
                                         double angelOfView, double sensorRotation) {
-        this.sensorPosition = sensorPosition;
         Point leftPoint = new Point();
         Point rightPoint = new Point();
 
-        double angleInRadian = Math.toRadians(angelOfView);
-        double sensorRotationInRadian = Math.toRadians(sensorRotation);
+        double angleInRadian = Math.toRadians( angelOfView );
+        double sensorRotationInRadian = Math.toRadians( sensorRotation );
 
-        leftPoint.x = (int) (Math.round(sensorPosition.x + Math.tan(angleInRadian / 2)) * visualRange);
-        leftPoint.y = (int) Math.round(sensorPosition.y + visualRange);
-        rightPoint.x = (int) (Math.round(sensorPosition.x - Math.tan(angleInRadian / 2)) * visualRange);
-        rightPoint.y = (int) Math.round(sensorPosition.y + visualRange);
+        leftPoint.x = (int) (Math.round( sensorPosition.x + Math.tan( angleInRadian / 2 ) ) * visualRange);
+        leftPoint.y = (int) Math.round( sensorPosition.y + visualRange );
+        rightPoint.x = (int) (Math.round( sensorPosition.x - Math.tan( angleInRadian / 2 ) ) * visualRange);
+        rightPoint.y = (int) Math.round( sensorPosition.y + visualRange );
 
-        leftPoint = rotate(leftPoint, sensorPosition, sensorRotationInRadian);
-        rightPoint = rotate(rightPoint, sensorPosition, sensorRotationInRadian);
+        leftPoint = rotate( leftPoint, sensorPosition, sensorRotationInRadian );
+        rightPoint = rotate( rightPoint, sensorPosition, sensorRotationInRadian );
 
-        Polygon triangle = new Polygon();
+        triangle = new Polygon();
         triangle.npoints = TRIANGLE_N;
         triangle.xpoints = new int[]{sensorPosition.x, leftPoint.x, rightPoint.x};
         triangle.ypoints = new int[]{sensorPosition.y, leftPoint.y, rightPoint.y};
@@ -57,26 +53,28 @@ public class CameraSensor extends SystemComponent implements ISensor {
 
     private Point rotate(Point point, Point sennsorLocation, double rotation) {
 
-        double x = sennsorLocation.x + (point.x - sennsorLocation.x) * Math.cos(rotation)
-                - (point.y - sennsorLocation.y) * Math.sin(rotation);
-        double y = sennsorLocation.y + (point.x - sennsorLocation.x) * Math.sin(rotation)
-                + (point.y - sennsorLocation.y) * Math.cos(rotation);
+        double x = sennsorLocation.x + (point.x - sennsorLocation.x) * Math.cos( rotation )
+                - (point.y - sennsorLocation.y) * Math.sin( rotation );
+        double y = sennsorLocation.y + (point.x - sennsorLocation.x) * Math.sin( rotation )
+                + (point.y - sennsorLocation.y) * Math.cos( rotation );
 
-        return new Point((int) x, (int) y);
+        return new Point( (int) x, (int) y );
     }
 
     @Override
     public void refreshSensor(Point newSensorPosition, double newSensorRotation) {
-        locateSensorTriangle(newSensorPosition,0, 0, newSensorRotation);
+        locateSensorTriangle( newSensorPosition, 0, 0, newSensorRotation );
     }
 
     @Override
     public List<WorldObject> detectedObjects(List<WorldObject> worldObjects) {
         List<WorldObject> list = new ArrayList<>();
-        for (WorldObject worldObject : worldObjects){
-            Rectangle rectangle = new Rectangle(worldObject.getX(), worldObject.getY(), worldObject.getWidth(), worldObject.getHeight());
-            if(triangle.intersects(rectangle))
-                list.add(worldObject);
+        for (WorldObject worldObject : worldObjects) {
+            Rectangle rectangle = new Rectangle( worldObject.getX(), worldObject.getY(),
+                    worldObject.getWidth(), worldObject.getHeight() );
+            if (triangle.intersects( rectangle )) {
+                list.add( worldObject );
+            }
         }
         return list;
     }
